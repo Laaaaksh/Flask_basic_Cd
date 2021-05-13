767ceb3212fe200e123641c298c8ea2888bef3b9
Laksh Sadhwani <47391874+Laaaaksh@users.noreply.github.com>
2021-05-13 18:19:56 +0530

    Added user.html for social networks

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
 app= Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hey you are back on the Home page"
+#how to map 2 different urls to the same function
+@app.route("/")
+@app.route("/<user>")
+def index(user=None):
+    return render_template("user.html", user=user)
 
-@app.route('/profile/<name>')
-def profile(name):
-    return render_template("profile.html", name=name)
+# @app.route('/')
+# def index():
+#     return "Hey you are back on the Home page"
+
+# @app.route('/profile/<name>')
+# def profile(name):
+#     return render_template("profile.html", name=name)
 
 
 if __name__ == '__main__':
